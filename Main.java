d0367bdd7b7874e8aa365df858c07f7a918464a5
Mohammad Jafar Jafar <jafarkashify@gmail.com>
2024-12-18 16:50:19 +0330

    Javadoc added to the file

--- Main.java
+++ Main.java
@@ -1,4 +1,15 @@
+/**
+ * Entry point for the Forest Animals Game.
+ * This class contains the main method to start the game.
+ * @author Mohammad Jafar Saberi
+ * @version 1.0
+ */
 public class Main {
+    /**
+     * Main method to launch the game.
+     *
+     * @param args Command-line arguments (not used in this application).
+     */
     public static void main(String[] args) {
         Management.play();
     }
